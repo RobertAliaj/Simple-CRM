04b4fb61a2f36f64bde0048ee82ad99f73ce94be
rOBI <robertaliaj97@gmail.com>
2023-07-21 17:44:03 +0200

    tutorial update

--- src/app/tutorial.service.ts
+++ src/app/tutorial.service.ts
@@ -16,7 +16,12 @@ export class TutorialService {
     {
       title: 'Welcome to my Simple CRM Web Application !',
       img: 'lets-go.webp',
-      description: 'Welcome to the Crypto Owners CRM! Here you can add and manage users, keep track of how many bitcoins your users have, and even send them a few more. This tutorial will guide you through the main functionalities of my web application. Let\'s dive in together, click Next to start the journey! ',
+      description: 'Welcome to the Crypto Owners CRM! Stay updated with the current BTC price, monitor the Bitcoin balance of other users, and even facilitate transfers to increase their holdings. This tutorial will guide you through the main functionalities of my web application. Let\'s dive in together, click Next to start the journey! ',
+    },
+    {
+      title: 'Login',
+      img: 'lets-go.webp',
+      description: 'Register to fully unlock all the features of the app. Alternatively, you can log in as a guest , though please note that this limits your access to some functionalities.',
     },
     {
       title: 'Dashboard',
@@ -26,27 +31,32 @@ export class TutorialService {
     {
       title: 'User',
       img: 'add-user.webp',
-      description: 'In the user area, you can have an overview of all your users and you can add a new one by clicking the plus button located at the bottom right.'
+      description: 'Overview of all users.'
     },
     {
       title: 'User',
       img: 'openuser.webp',
-      description: 'After you have created a user, you can click on the username to go to the \'User Details\'.'
+      description: 'You can click on the username to go to the \'User Details\'.'
+    },
+    {
+      title: 'Search',
+      img: 'openuser.webp',
+      description: 'Utilize the search bar to find specific users or transactions.'
     },
     {
       title: 'User Detail',
       img: 'user-detail.webp',
-      description: 'Here, you get an overview of the user details and the amount of Bitcoin the user has received up to now.'
+      description: ' Here, you get an overview of your details and the amount of Bitcoin you have received so far.'
     },
     {
       title: 'Edit User Detail',
       img: 'edit-user.webp',
-      description: 'Edit Personal User Details.'
+      description: 'You can change your personal data here.'
     },
     {
       title: 'Delete the User',
       img: 'delete-user.webp',
-      description: 'Delete the user by clicking the delete button.'
+      description: 'Or delete your account. (Only for registered users)'
     },
     {
       title: 'Send Bitcoins',
@@ -61,7 +71,12 @@ export class TutorialService {
     {
       title: 'Transactions',
       img: 'transaction1.webp',
-      description: 'In the \'Transactions\' section, you can view and track all completed transactions.'
+      description: 'In the \'Transactions\' section, you can review and follow every completed transaction, observing both the sender and the receiver.'
+    },
+    {
+      title: 'Transaction Chart',
+      img: 'transaction1.webp',
+      description: 'Each user\'s Bitcoin balance is neatly displayed in a chart.'
     },
     {
       title: 'News',
